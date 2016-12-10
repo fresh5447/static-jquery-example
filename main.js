00edc6baf232d3d3d0dedc46c2a576d327406fb6
fresh5447 <doug@kosmojo.com>
2016-12-09 18:29:30 -0700

    use button to trigger event

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-// $('.caption-text').text("helllooooooooo").css('background-color', 'orange');
+// when click event happens
 
+$('.caption-text').text("helllooooooooo").css('background-color', 'orange');
 
-var captionText = $('.caption-text').text("helllooooooooo");
-captionText.css('background-color', 'orange');
+// listening when event happens
+// run this block of code
+var greeting = function(){
+  alert('heyy')
+}
+
+$('.alert-btn').click(greeting);
