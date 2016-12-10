62e46828f965ca1737c96b1164b1e2fd084ee13e
fresh5447 <doug@kosmojo.com>
2016-12-09 18:18:50 -0700

    chaining example

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-$('.caption-text').text("helllooooooooo")
+// $('.caption-text').text("helllooooooooo").css('background-color', 'orange');
+
+
+var captionText = $('.caption-text').text("helllooooooooo");
+captionText.css('background-color', 'orange');
